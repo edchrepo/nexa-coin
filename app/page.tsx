931f1c9ada722c55b28566c5f248f6d280d5e307
Ellis <ellischung98@gmail.com>
2023-11-29 22:50:14 -0500

    added new component

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
+import CoinsTable from "@/components/CoinsTable";
+
 export default function Home() {
   return (
     <div className="bg-[#1f2128] flex justify-center">
       <div className="w-[80%]">
-        <div>
-          Overview
-        </div>
-        <div>
-          Table
-        </div>
+        <div>Overview</div>
+        <CoinsTable />
       </div>
     </div>
-  )
+  );
 }
