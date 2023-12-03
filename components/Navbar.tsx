1d1e142cb6310f5b928b79de8208c910233046a9
Edward Chung <edwardchung2024@gmail.com>
2023-12-03 02:45:35 -0500

    link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <div className="mt-5 flex justify-center">
       <div className="w-[80%] flex items-center justify-center space-x-2">
-        <button className="bg-[#2c2f36] px-10 py-3.5 rounded-lg">Coins</button>
-        <button className="px-7 py-3.5 rounded-lg">Portfolio</button>
+        <Link className="bg-[#2c2f36] px-10 py-3.5 rounded-lg" href="/">Coins</Link>
+        <Link className="px-7 py-3.5 rounded-lg" href="/portfolio">Portfolio</Link>
         <div className="grow"/>
         <input
           type="text"
