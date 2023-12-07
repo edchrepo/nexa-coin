a75fb5f9df656adfedd0612539a06bcd5825ed90
Ellis <ellischung98@gmail.com>
2023-12-07 16:52:28 -0500

    css updates to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import Image from "next/image";
-import * as Icons from "../icons"
+import * as Icons from "../icons";
 
 const Navbar = () => {
   const [activeLink, setActiveLink] = useState("home");
@@ -19,40 +19,61 @@ const Navbar = () => {
       <div className="flex items-center">
         <div
           className={`flex items-center mr-4 p-2 rounded cursor-pointer ${
-            activeLink === "home" ? "gradient-active" : "text-[#8e8e8e]"
+            activeLink !== "home" && "text-secondary"
           }`}
           onClick={() => setActiveLink("home")}
         >
-          <Image className="h-7 w-7 mr-2" src={activeLink === "home" ? Icons.Home : Icons.HomeStatic} alt="home"/>
+          <Image
+            className="h-7 w-7 mr-2"
+            src={activeLink === "home" ? Icons.Home : Icons.HomeStatic}
+            alt="home"
+          />
           <p>Home</p>
         </div>
         <div
           className={`flex items-center mr-4 p-2 rounded cursor-pointer ${
-            activeLink === "portfolio" ? "gradient-active" : "text-[#8e8e8e]"
+            activeLink !== "portfolio" && "text-secondary"
           }`}
           onClick={() => setActiveLink("portfolio")}
         >
-          <Image className="h-7 w-7 mr-2" src={activeLink === "portfolio" ? Icons.Stack : Icons.StackStatic} alt="portfolio"/>
+          <Image
+            className="h-7 w-7 mr-2"
+            src={activeLink === "portfolio" ? Icons.Stack : Icons.StackStatic}
+            alt="portfolio"
+          />
           <p>Portfolio</p>
         </div>
       </div>
 
       {/* Search and Settings Section */}
       <div className="flex items-center">
-        <input
-          type="text"
-          placeholder="Search..."
-          className="bg-[#181825] rounded-md mr-4 p-2 w-64 h-10"
-        />
-        <select className="bg-[#181825] rounded-md mr-4 p-2 h-10">
+        <div className="relative">
+          <div className="absolute inset-y-0 ml-3 flex items-center pointer-events-none">
+            <Image className="h-5 w-5" src={Icons.Search} alt="search" />
+          </div>
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-[#181825] border border-border rounded-md mr-4 p-2 pl-10 w-64 h-10"
+          />
+        </div>
+        <select className="bg-[#181825] border border-border rounded-md mr-4 p-2 h-10">
           <option value="USD">USD</option>
           <option value="EUR">EUR</option>
+          <option value="EUR">GBP</option>
+          <option value="EUR">JPY</option>
+          <option value="EUR">RMB</option>
+          <option value="EUR">KRW</option>
         </select>
         <button
-          className="bg-[#181825] rounded-md p-2 h-10"
+          className="flex items-center bg-[#181825] border border-border rounded-xl p-2 h-10"
           onClick={() => setDarkMode(!darkMode)}
         >
-          {darkMode ? <Image className="h-7 w-7 mr-2" src={Icons.Dark} alt="Dark"/> : <Image className="h-7 w-7 mr-2" src={Icons.Light} alt="Light"/>}
+          {darkMode ? (
+            <Image className="h-5 w-5" src={Icons.Dark} alt="Dark" />
+          ) : (
+            <Image className="h-5 w-5" src={Icons.Light} alt="Light" />
+          )}
         </button>
       </div>
     </nav>
