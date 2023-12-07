faaa6e3565d26c153aa69714237e1716dc095438
Edward Chung <edwardchung2024@gmail.com>
2023-12-07 17:31:56 -0500

    tabs finished

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-import ChartOverview from "../components/ChartOverview";
-
+import Tabs from "@/components/Tabs"
+import CurrencyStats from "@/components/CurrencyStats";
+import ChartOverview from "@/components/ChartOverview";
 import CoinsTable from "@/components/CoinsTable";
 
 export default function Home() {
   return (
     <div className="bg-[#13121a] flex-col justify-center w-[90%]">
+      <Tabs />
+      <CurrencyStats />
       <ChartOverview />
       <CoinsTable />
     </div>
---
+++ components/Tabs.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import React, { useState } from "react";
+
+const Tabs = () => {
+  const [activeTab, setActiveTab] = useState("coins");
+  return (
+    <div className="flex bg-[#181825] w-[25%] py-2 px-5 rounded-[15px]">
+      <div
+        className={`flex justify-center items-center 
+            ${
+              activeTab === "coins"
+                ? "bg-[#3c3c7e] border-2 border-[#6161cb]"
+                : "bg-[#232336]"
+            } p-4 rounded-lg w-[50%]`}
+        onClick={() => setActiveTab("coins")}
+      >
+        Coins
+      </div>
+      <div
+        className={`flex justify-center items-center 
+            ${
+              activeTab === "converter"
+                ? "bg-[#3c3c7e] border-2 border-[#6161cb]"
+                : "bg-[#232336]"
+            } p-4 rounded-lg w-[50%]`}
+        onClick={() => setActiveTab("converter")}
+      >
+        Converter
+      </div>
+    </div>
+  );
+};
+
+export default Tabs;
