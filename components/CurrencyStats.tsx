5db0da06d734cbb5a1d8bb2d65949d35b600aa22
Ellis <ellischung98@gmail.com>
2023-12-08 02:23:56 -0500

    added/styled button and text

--- components/CurrencyStats.tsx
+++ components/CurrencyStats.tsx
@@ -1,12 +1,40 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import * as Icons from "../icons";
 
 const CurrencyStats = () => {
+  const [compare, setCompare] = useState(false);
+
   return (
     <div>
-      <div className="text-secondary">
-        Select the currency to view statistics
+      <div className="flex justify-between text-secondary">
+        <div className="flex items-end">
+          Select the currency to view statistics
+        </div>
+        <button
+          className="flex items-center bg-[#232336] text-white rounded-md p-3 px-8"
+          onClick={() => setCompare(!compare)}
+        >
+          {compare ? (
+            <Image
+              className="h-7 w-7 -ml-1.5 mr-2"
+              src={Icons.Dark}
+              alt="Dark"
+            />
+          ) : (
+            <Image
+              className="h-7 w-7 -ml-1.5 mr-2"
+              src={Icons.Light}
+              alt="Light"
+            />
+          )}
+          {compare ? "Compare" : "Exit comparison"}
+        </button>
+
+        {/* Map each coin and set activeCoin. Carousel effect here */}
       </div>
-      <button className="rounded-md">Compare</button>
     </div>
   );
 };
