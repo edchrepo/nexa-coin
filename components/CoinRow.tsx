cd26b72e067da791ea9b343e0525bfcf3626ec9e
Ellis <ellischung98@gmail.com>
2023-12-11 03:02:19 -0500

    added + styled coin rows

---
+++ components/CoinRow.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import ProgressBar from "./ProgressBar";
+import { formatCurrency } from "@/utils/utils";
+import { CoinData } from "./CoinsTable";
+
+interface CoinProps {
+  coin: CoinData;
+  index: number;
+}
+
+const CoinRow: React.FC<CoinProps> = ({ coin, index }) => {
+  return (
+    <div className="grid grid-cols-48 gap-2 bg-[#181825] border-[#181825] rounded-md my-2 py-4 items-center">
+      <div className="col-span-2 text-center">{index + 1}</div>
+      <div className="col-span-6">
+        <img
+          src={coin.image}
+          className="w-8 h-8 inline-block"
+          alt={coin.name}
+        />{" "}
+        {coin.name} ({coin.symbol.toUpperCase()})
+      </div>
+      <div className="col-span-4">${coin.current_price}</div>
+      <div
+        className={`col-span-4 ${
+          coin.price_change_percentage_1h_in_currency > 0
+            ? "text-green-500"
+            : "text-red-500"
+        }`}
+      >
+        {Math.round(100 * coin.price_change_percentage_1h_in_currency) / 100}%
+      </div>
+      <div
+        className={`col-span-4 ${
+          coin.price_change_percentage_24h_in_currency > 0
+            ? "text-green-500"
+            : "text-red-500"
+        }`}
+      >
+        {Math.round(100 * coin.price_change_percentage_24h_in_currency) / 100}%
+      </div>
+      <div
+        className={`col-span-4 ${
+          coin.price_change_percentage_7d_in_currency > 0
+            ? "text-green-500"
+            : "text-red-500"
+        }`}
+      >
+        {Math.round(100 * coin.price_change_percentage_7d_in_currency) / 100}%
+      </div>
+      <div className="col-span-8">
+        {formatCurrency(coin.total_volume)} / {formatCurrency(coin.market_cap)}
+        <ProgressBar
+          progress={Math.min(
+            Math.round((coin.total_volume / coin.market_cap) * 100),
+            100
+          )}
+        />
+      </div>
+      <div className="col-span-8">
+        {formatCurrency(coin.circulating_supply)} /{" "}
+        {formatCurrency(coin.total_supply)}
+        <ProgressBar
+          progress={Math.round(
+            (coin.circulating_supply / coin.total_supply) * 100
+          )}
+        />
+      </div>
+      <div className="col-span-6">Spark chart for 7d</div>
+    </div>
+  );
+};
+
+export default CoinRow;
--- components/CoinsTable.tsx
+++ components/CoinsTable.tsx
@@ -1,10 +1,9 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import ProgressBar from "./ProgressBar";
-import { formatCurrency } from "@/utils/utils";
+import CoinRow from "./CoinRow";
 
-interface CoinData {
+export interface CoinData {
   id: string;
   symbol: string;
   name: string;
@@ -43,86 +42,20 @@ const CoinsTable = () => {
 
   return (
     <div>
-      <table className="table-fixed">
-        <thead>
-          <tr className="text-left border-b border-gray-500">
-            <th className="w-1/12">#</th>
-            <th className="w-1/12">Name</th>
-            <th className="w-1/12">Current Price</th>
-            <th className="w-1/12">1h</th>
-            <th className="w-1/12">24h</th>
-            <th className="w-1/12">7d</th>
-            <th className="w-1/6">24h Vol / Market Cap</th>
-            <th className="w-1/6">Circulating / Total Sup</th>
-            <th className="w-1/6">Last 7d</th>
-          </tr>
-        </thead>
-        <tbody>
-          {coins.map((coin, index) => (
-            <tr key={coin.id} className="border-b border-gray-500">
-              <td className="w-1/12">{index + 1}</td>
-              <td className="w-1/12">
-                <img src={coin.image} className="w-8 h-8 inline-block" />{" "}
-                {coin.name} ({coin.symbol.toUpperCase()})
-              </td>
-              <td className="w-1/12">${coin.current_price}</td>
-              <td
-                className={`w-1/12 ${
-                  coin.price_change_percentage_1h_in_currency > 0
-                    ? "text-green-500"
-                    : "text-red-500"
-                }`}
-              >
-                {Math.round(100 * coin.price_change_percentage_1h_in_currency) /
-                  100}
-                %
-              </td>
-              <td
-                className={`w-1/12 ${
-                  coin.price_change_percentage_24h_in_currency > 0
-                    ? "text-green-500"
-                    : "text-red-500"
-                }`}
-              >
-                {Math.round(
-                  100 * coin.price_change_percentage_24h_in_currency
-                ) / 100}
-                %
-              </td>
-              <td
-                className={`w-1/12 ${
-                  coin.price_change_percentage_7d_in_currency > 0
-                    ? "text-green-500"
-                    : "text-red-500"
-                }`}
-              >
-                {Math.round(100 * coin.price_change_percentage_7d_in_currency) /
-                  100}
-                %
-              </td>
-              <td className="w-1/6">
-                {formatCurrency(coin.total_volume)} /{" "}
-                {formatCurrency(coin.market_cap)}
-                <ProgressBar
-                  progress={Math.min(
-                    Math.round((coin.total_volume / coin.market_cap) * 100),
-                    100
-                  )}
-                />
-              </td>
-              <td className="w-1/6">
-                {formatCurrency(coin.circulating_supply)} /{" "}
-                {formatCurrency(coin.total_supply)}
-                <ProgressBar
-                  progress={Math.round(
-                    (coin.circulating_supply / coin.total_supply) * 100
-                  )}
-                />
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div className="grid grid-cols-48 gap-2 text-left text-secondary mt-20 mb-5">
+        <div className="col-span-2 text-center">#</div>
+        <div className="col-span-6">Name</div>
+        <div className="col-span-4">Price</div>
+        <div className="col-span-4">1h%</div>
+        <div className="col-span-4">24h%</div>
+        <div className="col-span-4">7d%</div>
+        <div className="col-span-8">24h Volume / Market Cap</div>
+        <div className="col-span-8">Circulating / Total Supply</div>
+        <div className="col-span-6">Last 7d</div>
+      </div>
+      {coins.map((coin, index) => (
+        <CoinRow key={coin.id} coin={coin} index={index} />
+      ))}
     </div>
   );
 };
