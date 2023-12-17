cae4b11ea033f5a708d95595bdbc5222c5a918d0
Ellis <ellischung98@gmail.com>
2023-12-16 23:04:49 -0500

    added clickable sort svgs for filtering

--- components/CoinsTable.tsx
+++ components/CoinsTable.tsx
@@ -3,6 +3,8 @@
 import { useState, useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../app/store/hooks";
 import { fetchCoinsData } from "../app/store/slices/coinsDataSlice";
+import Image from "next/image";
+import * as Icons from "../icons";
 import CoinRow from "./CoinRow";
 import InfiniteScroll from "react-infinite-scroll-component";
 
@@ -11,6 +13,10 @@ const CoinsTable = () => {
   const coins = useAppSelector((state) => state.coinsData);
   const [page, setPage] = useState(1);
   const hasMore = coins.length % 50 === 0;
+
+  const handleSort = () => {
+    console.log("test");
+  };
 
   const fetchMoreData = () => {
     setPage((prevPage) => prevPage + 1);
@@ -24,11 +30,51 @@ const CoinsTable = () => {
     <div>
       <div className="grid grid-cols-48 gap-2 text-left text-[#3c3c7e] dark:text-secondary mt-20 mb-5">
         <div className="col-span-2 text-center">#</div>
-        <div className="col-span-6">Name</div>
-        <div className="col-span-4">Price</div>
-        <div className="col-span-4">1h%</div>
-        <div className="col-span-4">24h%</div>
-        <div className="col-span-4">7d%</div>
+        <div className="flex items-center justify-left col-span-6">
+          <div className="mr-1">Name</div>
+          <Image
+            className="h-5 w-5 cursor-pointer"
+            src={Icons.Sort}
+            alt="sort"
+            onClick={handleSort}
+          />
+        </div>
+        <div className="flex items-center justify-left col-span-4">
+          <div className="mr-1">Price</div>
+          <Image
+            className="h-5 w-5 cursor-pointer"
+            src={Icons.Sort}
+            alt="sort"
+            onClick={handleSort}
+          />
+        </div>
+        <div className="flex items-center justify-left col-span-4">
+          <div className="mr-1">1h%</div>
+          <Image
+            className="h-5 w-5 cursor-pointer"
+            src={Icons.Sort}
+            alt="sort"
+            onClick={handleSort}
+          />
+        </div>
+        <div className="flex items-center justify-left col-span-4">
+          <div className="mr-1">24h%</div>
+          <Image
+            className="h-5 w-5 cursor-pointer"
+            src={Icons.Sort}
+            alt="sort"
+            onClick={handleSort}
+          />
+        </div>
+        <div className="flex items-center justify-left col-span-4">
+          <div className="mr-1">7d%</div>
+          <Image
+            className="h-5 w-5 cursor-pointer"
+            src={Icons.Sort}
+            alt="sort"
+            onClick={handleSort}
+          />
+        </div>
         <div className="col-span-8">24h Volume / Market Cap</div>
         <div className="col-span-8">Circulating / Total Supply</div>
         <div className="col-span-6">Last 7d</div>
