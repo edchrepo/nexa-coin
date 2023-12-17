ebc5d083e18f873b53fd1e949ea594dd5a22e3dd
Ellis <ellischung98@gmail.com>
2023-12-17 00:24:15 -0500

    coin table field filtering working

--- components/CoinsTable.tsx
+++ components/CoinsTable.tsx
@@ -5,17 +5,41 @@ import { useAppDispatch, useAppSelector } from "../app/store/hooks";
 import { fetchCoinsData } from "../app/store/slices/coinsDataSlice";
 import Image from "next/image";
 import * as Icons from "../icons";
+import { CoinData } from "../app/store/slices/coinsDataSlice";
 import CoinRow from "./CoinRow";
 import InfiniteScroll from "react-infinite-scroll-component";
+
+type SortKey = keyof CoinData;
 
 const CoinsTable = () => {
   const dispatch = useAppDispatch();
   const coins = useAppSelector((state) => state.coinsData);
+  const [sortedCoins, setSortedCoins] = useState<CoinData[]>([]);
+  const [sortConfig, setSortConfig] = useState({ key: "", direction: "" });
   const [page, setPage] = useState(1);
   const hasMore = coins.length % 50 === 0;
 
-  const handleSort = () => {
-    console.log("test");
+  const handleSort = (key: SortKey) => {
+    let direction = "ascending";
+    if (sortConfig.key === key && sortConfig.direction === "ascending") {
+      direction = "descending";
+    }
+    setSortConfig({ key, direction });
+
+    const sortedData = [...coins].sort((a, b) => {
+      if (key in a && key in b) {
+        if (a[key] < b[key]) {
+          return direction === "ascending" ? -1 : 1;
+        }
+        if (a[key] > b[key]) {
+          return direction === "ascending" ? 1 : -1;
+        }
+      }
+      return 0;
+    });
+
+    setSortedCoins(sortedData);
+    console.log(sortedCoins);
   };
 
   const fetchMoreData = () => {
@@ -25,6 +49,10 @@ const CoinsTable = () => {
   useEffect(() => {
     dispatch(fetchCoinsData(page));
   }, [dispatch, page]);
+
+  useEffect(() => {
+    setSortedCoins(coins);
+  }, [coins]);
 
   return (
     <div>
@@ -36,7 +64,7 @@ const CoinsTable = () => {
             className="h-5 w-5 cursor-pointer"
             src={Icons.Sort}
             alt="sort"
-            onClick={handleSort}
+            onClick={() => handleSort("name" as SortKey)}
           />
         </div>
         <div className="flex items-center justify-left col-span-4">
@@ -45,7 +73,7 @@ const CoinsTable = () => {
             className="h-5 w-5 cursor-pointer"
             src={Icons.Sort}
             alt="sort"
-            onClick={handleSort}
+            onClick={() => handleSort("current_price" as SortKey)}
           />
         </div>
         <div className="flex items-center justify-left col-span-4">
@@ -54,7 +82,9 @@ const CoinsTable = () => {
             className="h-5 w-5 cursor-pointer"
             src={Icons.Sort}
             alt="sort"
-            onClick={handleSort}
+            onClick={() =>
+              handleSort("price_change_percentage_1h_in_currency" as SortKey)
+            }
           />
         </div>
         <div className="flex items-center justify-left col-span-4">
@@ -63,7 +93,9 @@ const CoinsTable = () => {
             className="h-5 w-5 cursor-pointer"
             src={Icons.Sort}
             alt="sort"
-            onClick={handleSort}
+            onClick={() =>
+              handleSort("price_change_percentage_24h_in_currency" as SortKey)
+            }
           />
         </div>
         <div className="flex items-center justify-left col-span-4">
@@ -72,7 +104,9 @@ const CoinsTable = () => {
             className="h-5 w-5 cursor-pointer"
             src={Icons.Sort}
             alt="sort"
-            onClick={handleSort}
+            onClick={() =>
+              handleSort("price_change_percentage_7d_in_currency" as SortKey)
+            }
           />
         </div>
         <div className="col-span-8">24h Volume / Market Cap</div>
@@ -85,7 +119,7 @@ const CoinsTable = () => {
         hasMore={hasMore}
         loader={<p className="text-center mt-4">Loading more coins...</p>}
       >
-        {coins.map((coin, index) => (
+        {sortedCoins.map((coin, index) => (
           <CoinRow key={index} coin={coin} index={index} />
         ))}
       </InfiniteScroll>
