2ad0257df2766c3a05846a9cb9c878c326c600a2
Edward Chung <edwardchung2024@gmail.com>
2023-12-17 03:25:41 -0500

    rate limit API fix

--- app/store/slices/chartDataSlice.ts
+++ app/store/slices/chartDataSlice.ts
@@ -48,44 +48,49 @@ const initialState: ChartData = {
 // );
 
 export const fetchChartData = createAsyncThunk(
-  'chartData/fetchChartData',
+  "chartData/fetchChartData",
   async (args: ChartDataArgs, { rejectWithValue }) => {
-      const { timeFrame, selectedCoins } = args;
+    const { timeFrame, selectedCoins } = args;
 
-      // Default to 'bitcoin' if selectedCoins is empty
-      const coinsToFetch = selectedCoins.length === 0 ? ['bitcoin'] : selectedCoins;
+    // Default to 'bitcoin' if selectedCoins is empty
+    const coinsToFetch =
+      selectedCoins.length === 0 ? ["bitcoin"] : selectedCoins;
 
-      if (!process.env.NEXT_PUBLIC_API_CHART_URL) {
-          return rejectWithValue('API URL is not defined');
-      }
+    if (!process.env.NEXT_PUBLIC_API_CHART_URL) {
+      return rejectWithValue("API URL is not defined");
+    }
 
-      try {
-          // grab data for all selectedCoins
-          const fetchPromises = coinsToFetch.map(coinId =>
-              fetch(`${process.env.NEXT_PUBLIC_API_CHART_URL}${coinId}/market_chart?vs_currency=usd&days=${timeFrame}&interval=daily`)
-                  .then(response => {
-                      if (!response.ok) {
-                          throw new Error('Network error');
-                      }
-                      return response.json();
-                  })
-          );
+    try {
+      // grab data for all selectedCoins
+      const fetchPromises = coinsToFetch.map((coinId) =>
+        fetch(
+          `${process.env.NEXT_PUBLIC_API_CHART_URL}${coinId}/market_chart?vs_currency=usd&days=${timeFrame}&interval=daily&x_cg_demo_api_key=${process.env.NEXT_PUBLIC_API_KEY}`
+        ).then((response) => {
+          if (!response.ok) {
+            throw new Error("Network error");
+          }
+          return response.json();
+        })
+      );
 
-          const results = await Promise.all(fetchPromises);
+      const results = await Promise.all(fetchPromises);
 
-          // Combine data from all coins
-          const combinedData = results.reduce((acc, data, index) => {
-              acc.prices.push(data.prices);
-              acc.market_caps.push(data.market_caps);
-              acc.total_volumes.push(data.total_volumes);
-              return acc;
-          }, { prices: [], market_caps: [], total_volumes: [] });
+      // Combine data from all coins
+      const combinedData = results.reduce(
+        (acc, data, index) => {
+          acc.prices.push(data.prices);
+          acc.market_caps.push(data.market_caps);
+          acc.total_volumes.push(data.total_volumes);
+          return acc;
+        },
+        { prices: [], market_caps: [], total_volumes: [] }
+      );
 
-          console.log(combinedData)
-          return combinedData;
-      } catch (error) {
-          return rejectWithValue(error);
-      }
+      console.log(combinedData);
+      return combinedData;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
   }
 );
 
