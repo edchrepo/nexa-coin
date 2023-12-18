e010fe8337d6b2986eeb4191e99114e936eb82ba
Edward Chung <edwardchung2024@gmail.com>
2023-12-18 16:50:21 -0500

    refactor

--- app/store/slices/chartDataSlice.ts
+++ app/store/slices/chartDataSlice.ts
@@ -18,35 +18,6 @@ const initialState: ChartData = {
   total_volumes: [],
 };
 
-// Async thunk for fetching chart data
-// export const fetchChartData = createAsyncThunk(
-//   "chartData/fetchChartData",
-//   async (args: ChartDataArgs) => {
-//     const { timeFrame, selectedCoins } = args;
-//     const coinToFetch =
-//       selectedCoins.length === 0 ? "bitcoin" : selectedCoins[0];
-//     // Check if cached data is available and valid
-//     // const cachedData = getCache("chartDataCache");
-//     // if (cachedData) {
-//     //   return cachedData;
-//     // }
-
-//     if (!process.env.NEXT_PUBLIC_API_CHART_URL) {
-//       throw new Error("API URL is not defined");
-//     }
-
-//     const response = await fetch(
-//       `${process.env.NEXT_PUBLIC_API_CHART_URL}${coinToFetch}/market_chart?vs_currency=usd&days=${timeFrame}&interval=daily`
-//     );
-//     const data = await response.json();
-
-//     // Cache the new data with a specific expiration time (in minutes)
-//     // setCache("chartDataCache", data, 15);
-
-//     return data;
-//   }
-// );
-
 export const fetchChartData = createAsyncThunk(
   "chartData/fetchChartData",
   async (args: ChartDataArgs) => {
