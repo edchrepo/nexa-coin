3f51d248e1f7ec187ae93002ff6d20dd06ac6899
Ellis <ellischung98@gmail.com>
2023-12-28 17:09:09 -0500

    added assets with default data to portfolio page

--- app/pages/portfolio/page.tsx
+++ app/pages/portfolio/page.tsx
@@ -1,3 +1,23 @@
+import Asset from "@/app/components/Asset";
+
 export default function Portfolio() {
-  return <div>Portfolio Page</div>;
+  const assets = [1, 2, 3];
+
+  return (
+    <div className="bg-[#f3f5f9] dark:bg-[#13121a] w-[90%]">
+      <div className="flex justify-between p-4 mt-4">
+        <div className="flex items-center text-[#3c3c7e] dark:text-white text-2xl">
+          Portfolio
+        </div>
+        <button className="flex items-center bg-[#aaabe8] dark:bg-[#3c3c7e] text-[#3c3c7e] dark:text-white border-[#6161cb] shadow-whiteShadow rounded-md p-3 px-20">
+          Add Asset
+        </button>
+      </div>
+      <div className="p-3 mt-3">
+        {assets.map((index) => (
+          <Asset key={index} />
+        ))}
+      </div>
+    </div>
+  );
 }
