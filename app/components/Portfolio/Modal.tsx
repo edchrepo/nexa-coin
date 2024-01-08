1912f3ba448d701d8d5cf87276895046198b45de
Ellis <ellischung98@gmail.com>
2024-01-07 23:17:02 -0500

    added handlers for form fields

--- app/components/Portfolio/Modal.tsx
+++ app/components/Portfolio/Modal.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useAppSelector } from "@/app/store/hooks";
 import Image from "next/image";
 import * as Icons from "@/app/icons";
+import { getTodayDate } from "@/app/utils/utils";
 
 type ModalProps = {
   isOpen: boolean;
@@ -17,8 +18,8 @@ type SelectedCoinProps = {
 const Modal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
   const coins = useAppSelector((state) => state.coinsData);
   const [selectedCoin, setSelectedCoin] = useState<SelectedCoinProps>({});
-
-  if (!isOpen) return null;
+  const [purchasedAmount, setPurchasedAmount] = useState(0);
+  const [selectedDate, setSelectedDate] = useState("");
 
   const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const selectedCoinId = e.target.value;
@@ -27,6 +28,34 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
       setSelectedCoin(matchedCoin);
     }
   };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    if (parseFloat(value) > 0) {
+      setPurchasedAmount(parseFloat(value));
+    } else {
+      setPurchasedAmount(0);
+    }
+  };
+
+  const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedDate(e.target.value);
+  };
+
+  const clearModalState = () => {
+    setSelectedCoin({});
+    setPurchasedAmount(0);
+    setSelectedDate("");
+  };
+
+  const handleClose = () => {
+    clearModalState();
+    onClose();
+  };
+
+  const handleSubmit = () => {};
+
+  if (!isOpen) return null;
 
   return (
     <div
@@ -39,7 +68,7 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
           <span className="text-[#3c3c7e] dark:text-white text-lg">
             Select Coins
           </span>
-          <button onClick={onClose}>
+          <button onClick={handleClose}>
             <Image className="w-7" src={Icons.Close} alt="close" />
           </button>
         </div>
@@ -53,7 +82,9 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
               />
             </div>
             <span className="text-[#3c3c7e] dark:text-white text-2xl mt-3">
-              {selectedCoin.name} ({selectedCoin.symbol?.toUpperCase()})
+              {selectedCoin.name
+                ? `${selectedCoin.name} (${selectedCoin.symbol?.toUpperCase()})`
+                : "Select a coin"}
             </span>
           </div>
           <div className="flex flex-col w-full">
@@ -73,19 +104,32 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
               </select>
             </div>
             <div className="mb-4">
-              <select className="dark:bg-[#191925] text-secondary rounded-md p-2 w-full">
-                <option value="USD">Purchased amount</option>
-              </select>
+              <div className="flex justify-between dark:bg-[#191925] text-secondary rounded-md p-2 w-full">
+                <label>Purchased Amount</label>
+                <input
+                  type="number"
+                  className="dark:bg-[#191925] text-right placeholder:text-secondary"
+                  placeholder="0"
+                  onChange={handleInputChange}
+                />
+              </div>
             </div>
             <div className="mb-4">
-              <select className="dark:bg-[#191925] text-secondary rounded-md p-2 w-full">
-                <option value="date">Purchased date</option>
-              </select>
+              <div className="flex justify-between dark:bg-[#191925] text-secondary rounded-md p-2 w-full">
+                <label>Purchased Date</label>
+                <input
+                  type="date"
+                  className="dark:bg-[#191925] text-right"
+                  value={selectedDate}
+                  max={getTodayDate()}
+                  onChange={handleDateChange}
+                />
+              </div>
             </div>
             <div className="flex space-x-2 mt-4">
               <button
                 className="bg-[#aaabe8] dark:bg-[#232336] text-[#3c3c7e] dark:text-white rounded-md p-2 w-1/2"
-                onClick={onClose}
+                onClick={handleClose}
               >
                 Cancel
               </button>
--- app/utils/utils.ts
+++ app/utils/utils.ts
@@ -27,6 +27,14 @@ export function getCache(key: string) {
   return null;
 }
 
+export function getTodayDate(): string {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, "0"); // Months are 0-indexed
+  const day = String(today.getDate()).padStart(2, "0");
+  return `${year}-${month}-${day}`;
+}
+
 export function formatCurrency(value: number): string {
   let formattedValue: string;
 
