f4c7fd5c2bc8e41b92b6fd2694d56c07740ae0d5
Edward Chung <edwardchung2024@gmail.com>
2024-02-09 14:48:10 -0500

    mobile responsive modal

--- app/components/Portfolio/Modal.tsx
+++ app/components/Portfolio/Modal.tsx
@@ -135,7 +135,7 @@ const Modal: React.FC<ModalProps> = ({
           </button>
         </div>
         <div className="text-white flex">
-          <div className="flex flex-col justify-center items-center bg-white dark:bg-[#191932] rounded-md w-3/5 mr-6">
+          <div className="hidden md:flex flex-col justify-center items-center bg-white dark:bg-[#191932] rounded-md w-3/5 mr-6">
             <div className="flex bg-[#2c2c4a] p-3 rounded-lg">
               <img
                 src={assetToEdit ? assetToEdit.image : selectedCoin?.image}
