cd1bfa4596a484f297ce00ebe8bdb15846eab282
Edward Chung <edwardchung2024@gmail.com>
2024-02-15 16:49:54 -0500

    tab cursor-pointer

--- components/Tabs/Tabs.tsx
+++ components/Tabs/Tabs.tsx
@@ -11,7 +11,7 @@ const Tabs: React.FC<TabsProps> = ({ activeTab, setActiveTab }) => {
       {/* Desktop View */}
       <div className="hidden md:flex bg-white dark:bg-[#181825] w-[25%] py-2 px-5 rounded-[15px] mt-5">
         <div
-          className={`flex justify-center items-center 
+          className={`flex justify-center items-center cursor-pointer 
             ${
               activeTab === "coins"
                 ? "bg-[#aaabe8] dark:bg-[#3c3c7e] border-2 border-[#6161cb] shadow-whiteShadow"
@@ -22,7 +22,7 @@ const Tabs: React.FC<TabsProps> = ({ activeTab, setActiveTab }) => {
           Coins
         </div>
         <div
-          className={`flex justify-center items-center 
+          className={`flex justify-center items-center cursor-pointer 
             ${
               activeTab === "converter"
                 ? "bg-[#aaabe8] dark:bg-[#3c3c7e] border-2 border-[#6161cb] shadow-whiteShadow"
